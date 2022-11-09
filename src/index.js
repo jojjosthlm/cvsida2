23f470582723e02f8bd71e2a92eabe9d4f972af8
Joanna Olofsson <joanna.olofsson@chasacademy.se>
2022-11-09 09:43:59 +0100

    221109

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+const courseList = document.querySelector('.courseList');
+const workList = document.querySelector('.workList');
+
+
+async function getData() {
+    const url = '/cv.json';
+    let response = await fetch(url);
+    console.log('response: ', response);
+
+    if(response.ok) {
+        let json = await response.json();
+        console.log('json: ', json);
+        
+        json.courses.forEach(element => {
+            courseList.innerHTML += `
+            <li><h3>${element.course}</h3></li>
+            <li><p>${element.school}</p></li>
+            <li><p>${element.desc}</p></li>
+            <li><p>${element.period}</p></li>`;
+        });
+
+        json.work.forEach(element => {
+            workList.innerHTML += `
+            <li>
+            <p>${element.experience}</p>
+            </li>`;
+        });
+            
+    } else {
+        console.log("HTTP-Error: " + response.status);
+    }
+}
+
+getData();
+
 const modal = document.querySelector('.modal');
 const trigger = document.querySelector('.trigger');
 const closeButton = document.querySelector('.close-button');
@@ -15,3 +50,4 @@ function windowsOnclick(event) {
 trigger.addEventListener('click', toggleModal); //Här är något felfel..
 closeButton.addEventListener('click', toggleModal);
 window.addEventListener('click', windowsOnclick);
+
