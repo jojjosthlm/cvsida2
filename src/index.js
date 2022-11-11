bc232074c05a70b5fd185b942d0bb9d5f4a9f17c
Joanna Olofsson <joanna.olofsson@chasacademy.se>
2022-11-11 14:33:30 +0100

    221111

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ async function getData() {
         
         json.courses.forEach(element => {
             courseList.innerHTML += `
-            <h3>${element.course}</h3>
+            <h4>${element.course}</h4>
             <li>${element.school}</li>
             <li>${element.desc}</li>
             <li>${element.period}</li>`;
@@ -40,20 +40,3 @@ async function getData() {
 
 getData();
 
-const modal = document.querySelector('.modal');
-const trigger = document.querySelector('.trigger');
-const closeButton = document.querySelector('.close-button');
-
-function toggleModal(){
-    modal.classList.toggle('show-modal');
-}
-
-function windowsOnclick(event) {
-    if (event.target === modal){
-        toggleModal();
-    }
-}
-
-trigger.addEventListener('click', toggleModal); //Här är något felfel..
-closeButton.addEventListener('click', toggleModal); // Kan detta vara felet, att addEventListner används två gånger???
-window.addEventListener('click', windowsOnclick);
